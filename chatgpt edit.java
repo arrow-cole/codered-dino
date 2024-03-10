d79605616a47af451f2a3070f4a0749948a06002
arrow-cole <145709167+arrow-cole@users.noreply.github.com>
2024-03-10 12:11:25 -0700

    Update

---
+++ chatgpt edit.java
@@ -0,0 +1,15 @@
+String dino = "Dr Dino";
+Boolean cheeseburgerInTummy = false;
+
+if (cheeseburgerInTummy) {
+    dino.equals("happy");
+} else {
+    dino.equals("mad");
+}
+
+String popFlavor = "Code Red";
+if (popFlavor.equals("Code Red")) {
+    dino.equals("fueled");
+}
+
+System.out.println(dino + " gets fueled");
