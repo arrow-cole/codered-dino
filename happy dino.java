17fce610ca8c64b2c7d70c739f88438153ac7928
arrow-cole <145709167+arrow-cole@users.noreply.github.com>
2024-03-10 11:19:25 -0700

    Update happy dino.java

--- happy dino.java
+++ happy dino.java
@@ -2,3 +2,6 @@ String dino = "Dr Dino";
 Boolean cheeseburgerInTummy = false;
 if cheeseburgerInTummy = true then dino.equals("happy");
  else dino.equals("mad");
+
+if pop flavor = codered;
+dino = fueled;
