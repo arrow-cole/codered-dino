4a72978d90075612e2ffa1bde052cd4b8320c22d
audreymarsman15 <143760036+audreymarsman15@users.noreply.github.com>
2024-03-11 11:17:09 -0400

    Update happy dino.java
    I got carried away.

--- happy dino.java
+++ happy dino.java
@@ -1,9 +1,41 @@
-String dino = "Dr Dino";
-Boolean cheeseburgerInTummy = false;
-if cheeseburgerInTummy = true then dino.equals("happy");
- else dino.equals("mad");
-
-String popFlavor = "Code Red";
-if popFlavor.equals("Code Red")
- then dino.equals("fueled");
-System.out.println(dino gets fueled);
+public class Main {
+    private static class DrDino {
+        String name = "Dr. Dino";
+        boolean happy = false;
+        boolean cheeseburgerInTummy = false;
+        boolean fueled = false;
+        public void eatCheeseburger(Cheeseburger cheezyBurger) {
+            if (cheezyBurger.isGood()) {
+                cheeseburgerInTummy = true;
+                happy = true;
+            }
+        }
+        public Cheeseburger makeCheeseburger() {
+            Cheeseburger cheezyBurger = new Cheeseburger();
+            return cheezyBurger;
+        }
+        public void drinkRocketFuel(RocketFuel fuelz) {
+            if (fuelz.getFlavor().equals("Code Red")) {
+                fueled = true;
+            }
+        }
+    }
+    private static class Cheeseburger {
+        public boolean isGood() {
+            return true;
+        }
+    }
+    private static class RocketFuel {
+        String flavor = "Code Red";
+        public String getFlavor() {
+            return flavor;
+        }
+    }
+    public static void main(String[] args) {
+        DrDino dino = new DrDino();
+        Cheeseburger cheezyBurger = dino.makeCheeseburger();
+        dino.eatCheeseburger(cheezyBurger);
+        RocketFuel fuelz = new RocketFuel();
+        dino.drinkRocketFuel(fuelz);
+    }
+}
